1dd1fbd72413478d21a400d8f1068fa6779d93fd
deepak <deepak@kreatio.com>
2018-09-05 19:54:45 +0530

    Cleanup test case

--- tests/unit/message.js
+++ tests/unit/message.js
@@ -51,13 +51,12 @@ QUnit.test("Send and receive a message with a multi-byte UTF8 string", function 
   client.connect(TEST.login, TEST.password,
     function () {
       client.subscribe(TEST.destination, function (message) {
-        assert.equal(message.body.replace(/^"(.+(?="$))"$/, '$1'), payload);
-        assert.equal(message.body.length, 28);
+        assert.equal(message.body, payload);
         client.disconnect();
 
         done();
       });
 
-      client.send(TEST.destination, {}, JSON.stringify(payload));
+      client.send(TEST.destination, {}, payload);
     });
 });
